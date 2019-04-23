56eb5410899cd5ebf4d8bb9c0cdec514722aaafc
andreiuul <andreiuul@yahoo.com>
2019-04-23 22:24:56 +0300

    5. se da un numar natural n. afisati in ordine descrescatoare, primele n numere naturale nenule.

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         int i;
-        int countPar = 0;
-        int countImpar = 0;
-        int[] vector = new int[]{1,3,5,12,634,32,33,55,23,0,9};
-        for(i=0;i<vector.length; i++){
-            if(vector[i]%2 == 1){
-                countImpar++;
-            }else
-                countPar++;
+        int n = 7;
+        for(i=n;i>0;i--){
+            System.out.print(i+ " ");
         }
-            System.out.println("pare " + countPar);
-
-            System.out.println("impare " + countImpar);
-
-
-    }
+}
 }
