1a31f4d213337240eac924c5adada3e9440dfcb2
andreiuul <andreiuul@yahoo.com>
2019-04-23 22:21:45 +0300

    4. se dau n numere naturale. sa se calculeze cate dintre ele sunt pare si cate impare

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,18 @@ public class Main {
     public static void main(String[] args) {
 
         int i;
-        int max = -1;
+        int countPar = 0;
+        int countImpar = 0;
         int[] vector = new int[]{1,3,5,12,634,32,33,55,23,0,9};
         for(i=0;i<vector.length; i++){
-            if(vector[i] > max && vector[i]%2 == 1){
-                max = vector[i];
-            }
+            if(vector[i]%2 == 1){
+                countImpar++;
+            }else
+                countPar++;
         }
-        if(max == -1){
-            System.out.println("no");
-        }else
-            System.out.println(max);
+            System.out.println("pare " + countPar);
+
+            System.out.println("impare " + countImpar);
 
 
     }
