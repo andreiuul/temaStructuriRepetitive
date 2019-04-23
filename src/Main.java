6e826e43b17e7246399f2daf68285b6c6eaaa52c
andreiuul <andreiuul@yahoo.com>
2019-04-23 22:36:13 +0300

    7. se da un numar natural n. afisati suma cifrelor numarului 2 * 3 ^ n (2 inmultit cu 3 la puterea n).

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,18 @@ public class Main {
 
         int i;
         int n = 7;
-        for(i=0;i<n;i++){
-            System.out.print((2*i+1)+ " ");
+        int digits = 2;
+        int sum = 0;
+        for(i = 0; i < n; i++){
+            digits*=3;
         }
+
+        //System.out.println(digits);
+
+        while(digits != 0){
+            sum+=(digits%10);
+            digits/=10;
+        }
+        System.out.println(sum);
 }
 }
