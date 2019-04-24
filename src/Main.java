8f2344407ecc1cd32b3c2bd084283502c1829115
andreiuul <andreiuul@yahoo.com>
2019-04-24 18:12:15 +0300

    18. se da un caracter n (char). sa afiseze daca n este o litera ce reprezinta punctele cardinale.

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-       int n = 1234;
-       int m = 0;
-       int sumPar = 0;
-       int sumImpar = 0;
-       int position = 0;
+        char n = 'S';
 
-       while(n != 0){
-           if(position % 2 == 0) {
-               m += n % 10;
-           }else {
-               m -= n % 10;
-           }
-           n /= 10;
-           position++;
-       }
-        System.out.println(m);
+        if (n == 'N' || n == 'n' || n == 'S' || n == 's' || n == 'E' || n == 'e' || n == 'W' || n == 'w' || n == 'V' || n == 'v') {
+            System.out.println("reprezinta un punct cardinal");
+        } else {
+            System.out.println("nah bruh");
+        }
     }
 }
