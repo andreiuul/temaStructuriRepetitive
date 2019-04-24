2dd6f309634af7b36d41de35e4e60a502258c75a
andreiuul <andreiuul@yahoo.com>
2019-04-24 17:26:06 +0300

    12. se da un numar 0 <= n <= 11 (lunile anului, unde 0 -> ianuarie, etc). afisati un mesaj spunand in ce anotimp e luna reprezentata de n. la fel, scrieti ambele variante (switch si if).

--- src/Main.java
+++ src/Main.java
@@ -5,76 +5,73 @@ public class Main {
         int n = 5;
 
         //v1
-        if(n>=0 && n<=9){
-            if(n == 0){
-                System.out.println("zero");
-            }else if(n == 1){
-                System.out.println("unu");
-            }else if(n == 2){
-                System.out.println("doi");
-            }else if(n == 3){
-                System.out.println("trei");
-            }else if(n == 4){
-                System.out.println("patru");
-            }else if(n == 5){
-                System.out.println("cinci");
-            }else if(n == 6){
-                System.out.println("sase");
-            }else if(n == 7){
-                System.out.println("sapte");
-            }else if(n == 8){
-                System.out.println("opt");
-            }else if(n == 9){
-                System.out.println("noua");
+        if(n>=0 && n<=11){
+            if(n>=2 && n<=4){
+                System.out.println("Primavara");
+            }else if(n>=5 && n<=7 ){
+                System.out.println("Vara");
+            }else if(n >= 8 && n<=10){
+                System.out.println("Toamna");
+            }else {
+                System.out.println("Iarna");
             }
+
         }else{
-            System.out.println("numar gresit");
+            System.out.println("nah bruh");
         }
 
         //v2
         switch(n){
             case 0:{
-                System.out.println("zero");
+                System.out.println("Iarna");
                 break;
             }
             case 1:{
-                System.out.println("unu");
+                System.out.println("Iarna");
                 break;
             }
             case 2:{
-                System.out.println("doi");
+                System.out.println("Primavara");
                 break;
             }
             case 3:{
-                System.out.println("trei");
+                System.out.println("Primavara");
                 break;
             }
             case 4:{
-                System.out.println("patru");
+                System.out.println("Primavara");
                 break;
             }
             case 5:{
-                System.out.println("cinci");
+                System.out.println("Vara");
                 break;
             }
             case 6:{
-                System.out.println("sase");
+                System.out.println("Vara");
                 break;
             }
             case 7:{
-                System.out.println("sapte");
+                System.out.println("Vara");
                 break;
             }
             case 8:{
-                System.out.println("opt");
+                System.out.println("Toamna");
                 break;
             }
             case 9: {
-                System.out.println("noua");
+                System.out.println("Toamna");
+                break;
+            }
+            case 10: {
+                System.out.println("Toamna");
+                break;
+            }
+            case 11: {
+                System.out.println("Iarna");
                 break;
             }
             default:{
-                System.out.println("numar gresit");
+                System.out.println("nah bruh");
             }
         }
     }
