d6ad20e6518e097860dd8aaaf75c55212c092163
andreiuul <andreiuul@yahoo.com>
2019-04-24 17:51:14 +0300

    15. se dau doua numere n si m, scrieti un program care afiseaza "true" sau "false" (daca n este inversul lui m). exemplu: n = 12, m = 21 => true; n = 10, m = 1 => false

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
 public class Main {
 
     public static void main(String[] args) {
 
-       int n = 5;
-       int ok = 0;
+       int n = 21;
+       int m = 22;
+       int nInv = 0;
 
-       while(true){
-           if(ok<n){
-               System.out.println("going");
-               ok++;
-           }
-       }
+        while(n != 0){
+              nInv = n%10 + nInv*10;
+              n/=10;
+        }
+        if(nInv == m){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
     }
 }
