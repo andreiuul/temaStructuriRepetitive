5531593ca458fa89a6ca3257c54db2bd924ebff4
andreiuul <andreiuul@yahoo.com>
2019-04-24 17:30:22 +0300

    13. se da un numar n. scrieti un program care va afisa de n ori mesajul "go", urmat de gata.

--- src/Main.java
+++ src/Main.java
@@ -2,77 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int n = 5;
-
-        //v1
-        if(n>=0 && n<=11){
-            if(n>=2 && n<=4){
-                System.out.println("Primavara");
-            }else if(n>=5 && n<=7 ){
-                System.out.println("Vara");
-            }else if(n >= 8 && n<=10){
-                System.out.println("Toamna");
-            }else {
-                System.out.println("Iarna");
-            }
-
-        }else{
-            System.out.println("nah bruh");
-        }
-
-        //v2
-        switch(n){
-            case 0:{
-                System.out.println("Iarna");
-                break;
-            }
-            case 1:{
-                System.out.println("Iarna");
-                break;
-            }
-            case 2:{
-                System.out.println("Primavara");
-                break;
-            }
-            case 3:{
-                System.out.println("Primavara");
-                break;
-            }
-            case 4:{
-                System.out.println("Primavara");
-                break;
-            }
-            case 5:{
-                System.out.println("Vara");
-                break;
-            }
-            case 6:{
-                System.out.println("Vara");
-                break;
-            }
-            case 7:{
-                System.out.println("Vara");
-                break;
-            }
-            case 8:{
-                System.out.println("Toamna");
-                break;
-            }
-            case 9: {
-                System.out.println("Toamna");
-                break;
-            }
-            case 10: {
-                System.out.println("Toamna");
-                break;
-            }
-            case 11: {
-                System.out.println("Iarna");
-                break;
-            }
-            default:{
-                System.out.println("nah bruh");
-            }
-        }
+       int n = 5;
+       int i;
+       for(i = 0; i < n; i++){
+           System.out.print("go ");
+       }
+       System.out.print("gata"); // ?
     }
 }
