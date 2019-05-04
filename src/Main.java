abe2e302bf5f6262f9bbc47fbb791fca279624c5
andreiuul <andreiuul@yahoo.com>
2019-05-04 17:33:08 +0300

    19. se da un numar care reprezeinta CNP-ul unei persoane si are urmatorul format => SAALLZZJJNNNC. sa se scrie un program care afiseaza sexul, luna si anul nasterii unei, stiind ca : S => sexul : masculin / feminin 1 / 2 - nascuti intre 1 ianuarie 1900 si 31 decembrie 1999 3 / 4 - nascuti intre 1 ianuarie 1800 si 31 decembrie 1899 5 / 6 - nascuti intre 1 ianuarie 2000 si 31 decembrie 2099 7 / 8 - pentru persoanele straine rezidente in Romania. AA => numar format din 2 cifre si reprezinta ultimele 2 cifre din anul nasterii LL => luna (exemplu 05 => mai) ZZ => ziua (exemplu 09 = 9) exemplu : cnp = 1900510...... se va afisa mesajul "baiat nascut pe 10 mai 1990" 	  cnp = 2890205...... se va afisa mesajul "fata nascuta pe 5 februarie 1989"

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,95 @@ public class Main {
 
     public static void main(String[] args) {
 
-        char n = 'S';
+        long n = 2960223326598L;
 
-        if (n == 'N' || n == 'n' || n == 'S' || n == 's' || n == 'E' || n == 'e' || n == 'W' || n == 'w' || n == 'V' || n == 'v') {
-            System.out.println("reprezinta un punct cardinal");
-        } else {
-            System.out.println("nah bruh");
+        n /= 1000000;
+        int zi = (int)n%100;
+        int luna = ((int)n/100) % 100;
+        int an = ((int)n/10000) % 100;
+        int sex = (int)n/1000000;
+        switch(sex){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+                {
+                System.out.print("baiat nascut ");
+                break;
+            }
+            case 2:
+            case 4:
+            case 6:
+            case 8:
+                {
+                System.out.print("fata nascuta ");
+                break;
+            }
+            default:{
+                System.out.println("numar sex invalid ");
+            }
+        }
+
+        if(zi>0 && zi < 32){
+            System.out.print("pe "+ zi+" ");
+        }
+
+        switch(luna){
+            case 1: {
+                System.out.print("ianuarie ");
+                break;
+            }
+            case 2: {
+                System.out.print("februarie ");
+                break;
+            }
+            case 3: {
+                System.out.print("martie ");
+                break;
+            }
+            case 4: {
+                System.out.print("aprilie ");
+                break;
+            }
+            case 5: {
+                System.out.print("mai ");
+                break;
+            }
+            case 6: {
+                System.out.print("iunie ");
+                break;
+            }
+            case 7: {
+                System.out.print("iulie ");
+                break;
+            }
+            case 8: {
+                System.out.print("august ");
+                break;
+            }
+            case 9: {
+                System.out.print("septembrie ");
+                break;
+            }
+            case 10: {
+                System.out.print("octombrie ");
+                break;
+            }
+            case 11: {
+                System.out.print("noiembrie ");
+                break;
+            }
+            case 12: {
+                System.out.print("decembrie ");
+                break;
+            }
+        }
+        if(sex<3 || sex >6) {
+            System.out.print("19" + an);
+        }else if(sex<5){
+            System.out.print("18" + an);
+        }else if(sex<7){
+            System.out.print("20" + an);
         }
     }
 }
